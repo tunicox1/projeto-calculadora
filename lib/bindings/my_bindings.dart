52ea1b9c7d397104ac618b55edab271aa3003ff2
Cleanio <cleaniotqpaiva@gmail.com>
2023-05-15 21:55:34 -0300

    Commit calculadora dark/light

---
+++ lib/bindings/my_bindings.dart
@@ -0,0 +1,11 @@
+import 'package:calculadora/controller/calculate_controller.dart';
+import 'package:calculadora/controller/theme_controller.dart';
+import 'package:get/instance_manager.dart';
+
+class MyBindings implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => CalculateController());
+    Get.lazyPut(() => ThemeController());
+  }
+}
